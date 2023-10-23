f8138b0b93dda42d19bfd63a0f3e099caca2457d
JavedZ <54533579+JavedZ@users.noreply.github.com>
2023-10-23 14:00:14 +0200

    first Commit

---
+++ Assets/Scripts/DetectTile.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class DetectTile : MonoBehaviour
+{
+    private MovePlayer movePlayer;
+    private string currentPlayerTile;
+
+    private void Awake()
+    {
+        movePlayer = GetComponent<MovePlayer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(Time.timeScale != 0)
+        {
+            if (other.gameObject.CompareTag("Tile"))
+            {
+                currentPlayerTile = other.gameObject.name;
+                if (currentPlayerTile == movePlayer.GetSelectedTileName())
+                {
+                    Debug.Log("Tile underneath is: " + currentPlayerTile);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float speed = 2f;
+    private Camera cameraVar;
+    private Vector3 targetPosition;
+    private Vector3 currentPosition;
+    private string tileName;
+
+    private void Awake()
+    {
+        cameraVar = Camera.main;
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Mouse mouse = Mouse.current;
+        if(Time.timeScale >= 1.0f)
+        {
+            if (mouse.leftButton.wasPressedThisFrame)
+            {
+                Vector3 mousePosition = mouse.position.ReadValue();
+                Ray ray = cameraVar.ScreenPointToRay(mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    targetPosition = new Vector3(hit.transform.position.x, 1, hit.transform.position.z);
+                    tileName = hit.transform.name;
+                    Debug.Log("Tile selected: " + tileName);
+                }
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        currentPosition = targetPosition;
+    }
+
+    public string GetSelectedTileName()
+    {
+        return tileName;
+    }
+}
